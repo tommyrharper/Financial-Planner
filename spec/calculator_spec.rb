b5650a5641e6b917ec536fc007bfe0e728ba81d8
tommyrharper <tommyrharper@gmail.com>
2020-04-01 19:29:55 +0100

    test failed - cannot count years

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -9,4 +9,10 @@ describe Calculator do
     calculator = Calculator.new(10_000, 2020)
     expect(calculator.calculate(100, 1.1, 2)).to eq 14872
   end
+
+  it 'should be able to return an array of the correct list of years' do
+    calculator = Calculator.new(10_000, 2020)
+    calculator.calculate(100, 1.1, 2)
+    expect(calculator.years).to eq [2020, 2021, 2022]
+  end
 end
