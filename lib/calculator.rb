610c88c942ca4ddf6f527c2e07a207f62a25bc3b
tommyrharper <tommyrharper@gmail.com>
2020-04-01 19:27:52 +0100

    test passed - can now calculate from within the calculator class

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,8 +1,25 @@
 require_relative 'graph'
 
 class Calculator
-  
-  
+  def initialize(starting_cash, year = 2020)
+    @starting_cash = starting_cash
+    @year = year
+  end
+
+  def calculate(saving_pm, interest_rate_pa, years, starting_cash = @starting_cash, saving_increase = 0)
+    i = 2020
+    total = starting_cash
+    years.times {
+      i += 1 
+      total += saving_pm * 12
+      total = total*interest_rate_pa
+      saving_pm = saving_pm + saving_increase
+      #@total << total
+      #@year << i
+    }
+    total = total.round
+  end
+
 end
 
 def calculate(saving_pm, interest_rate_pa, years, starting_cash, saving_increase)
@@ -16,5 +33,5 @@ def calculate(saving_pm, interest_rate_pa, years, starting_cash, saving_increase
     #@total << total
     #@year << i
   }
-  total
+  total = total.round
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,7 +5,7 @@ describe Calculator do
     expect(calculate(100, 1.1, 2, 0, 0)).to eq 2772
   end
 
-  it 'should be able to initialize with starting year and amount to invest' do
+  it 'should be able to initialize with starting year and amount to invest and then calculate' do
     calculator = Calculator.new(10_000, 2020)
     expect(calculator.calculate(100, 1.1, 2)).to eq 14872
   end
