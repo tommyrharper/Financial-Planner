37fc57e8f184c37c28d88175ad052cedd2402d56
tommyrharper <tommyrharper@gmail.com>
2020-04-01 19:38:18 +0100

    test passed - array of total value each year created

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,7 +2,7 @@ require_relative 'graph'
 
 class Calculator
 
-  attr_reader :years
+  attr_reader :years, :totals
 
   STARTING_YEAR = 2020
 
@@ -10,6 +10,7 @@ class Calculator
     @starting_cash = starting_cash
     @years = [STARTING_YEAR]
     @year = STARTING_YEAR
+    @totals = [starting_cash]
   end
 
   def calculate(saving_pm, interest_rate_pa, years, starting_cash = @starting_cash, saving_increase = 0)
@@ -19,7 +20,7 @@ class Calculator
       total += saving_pm * 12
       total = total*interest_rate_pa
       saving_pm = saving_pm + saving_increase
-      #@total << total
+      @totals << total.round
       @years << @year
     }
     @YEAR = STARTING_YEAR
