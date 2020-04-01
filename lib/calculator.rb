043eddabb74cd102f866c41c827332499033fa5c
tommyrharper <tommyrharper@gmail.com>
2020-04-01 19:08:51 +0100

    test passed - calculation is accurate

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,20 @@ require_relative 'graph'
 
 class Calculator
 
+  
+  
+end
+
+def calculate(saving_pm, interest_rate_pa, years, starting_cash, saving_increase)
+
+  total = starting_cash
+
+  years.times {
+    total += saving_pm * 12
+    total = total*interest_rate_pa
+    saving_pm = saving_pm + saving_increase
+  }
+
+  total
+
 end
