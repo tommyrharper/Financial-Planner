41f720f92caf0cb15898b7422d84d011362ed7de
tommyrharper <tommyrharper@gmail.com>
2020-04-01 19:59:42 +0100

    refactored code in calculator.rb

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -3,39 +3,41 @@ require_relative 'graph'
 class Calculator
 
   attr_reader :years, :totals
+  attr_accessor 
 
   STARTING_YEAR = 2020
+  DEFAULT_INTEREST_RATE = 1.1 # 10%
+  DEFAULT_SAVING_INCREASE_PA = 0
+  DEFAULT_SAVING_PM = 0
 
-  def initialize(starting_cash)
+  def initialize(starting_cash, saving_pm = DEFAULT_SAVING_PM)
     @starting_cash = starting_cash
     @years = [STARTING_YEAR]
     @year = STARTING_YEAR
     @totals = [starting_cash]
+    @interest_rate_pa = DEFAULT_INTEREST_RATE
+    @saving_increase_pa = DEFAULT_SAVING_INCREASE_PA
+    @saving_pm = saving_pm
   end
 
-  def calculate(saving_pm, interest_rate_pa, years, starting_cash = @starting_cash, saving_increase = 0)
-    total = starting_cash
+  def calculate(years, saving_pm = @saving_pm, interest_rate_pa = @interest_rate_pa)
+    total = @starting_cash
+
     years.times {
       @year += 1 
       total += saving_pm * 12
-      total = total*interest_rate_pa
-      saving_pm = saving_pm + saving_increase
+      total = total * @interest_rate_pa
+      saving_pm = saving_pm + @saving_increase_pa
       @totals << total.round
       @years << @year
     }
-    @YEAR = STARTING_YEAR
+
+    @year = STARTING_YEAR
     total = total.round
+
   end
 
-
-
-
 end
-
-
-
-
-
 
 def calculate(saving_pm, interest_rate_pa, years, starting_cash, saving_increase)
   i = 2020
@@ -45,8 +47,6 @@ def calculate(saving_pm, interest_rate_pa, years, starting_cash, saving_increase
     total += saving_pm * 12
     total = total*interest_rate_pa
     saving_pm = saving_pm + saving_increase
-    #@total << total
-    #@year << i
   }
   total = total.round
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,23 +7,23 @@ describe Calculator do
 
   it 'should be able to initialize with amount to invest and then calculate' do
     calculator = Calculator.new(10_000)
-    expect(calculator.calculate(100, 1.1, 2)).to eq 14872
+    expect(calculator.calculate(2, 100)).to eq 14872
   end
 
   it 'should be able to return an array of the correct list of years' do
     calculator = Calculator.new(10_000)
-    calculator.calculate(100, 1.1, 2)
+    calculator.calculate(2, 100)
     expect(calculator.years).to eq [2020, 2021, 2022]
   end
 
   it 'should be able to return an array of total values each year' do
     calculator = Calculator.new(10_000)
-    calculator.calculate(100, 1.1, 2)
+    calculator.calculate(2, 100)
     expect(calculator.totals).to eq [10000, 12320, 14872]
   end
 
   it 'should allow the default interest rate to be 1.1' do
     calculator = Calculator.new(0)
-    expect(calculator.calculate(100, 2)).to eq 2772
+    expect(calculator.calculate(2, 100)).to eq 2772
   end
 end
